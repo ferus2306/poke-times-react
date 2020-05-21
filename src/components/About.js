7735ee5099ac216aa99e8bb717b5e69ef5897053
Firus Hanov <d00363593@dmail.dixie.edu>
2020-05-20 18:24:02 -0600

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+        <div className='container'>
+            <h4 className='center'>About</h4>
+            <p>lorem test About</p>
+        </div>
+    )
+}
+
+export default About
