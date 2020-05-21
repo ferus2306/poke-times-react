c891b1fa6ef70b76ac301d6269aa8cb8bb5f1bd0
Firus Hanov <d00363593@dmail.dixie.edu>
2020-05-21 14:15:59 -0600

    axios api implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
 
-const Home = () => {
-    return (
-        <div className='container'>
-            <h4 className='center'>Home</h4>
-            <p>lorem test Home</p>
-        </div>
-    )
+class Home extends Component {
+
+    state = {
+        posts: [ ]
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        // returns a promise since it is asychnonious
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    posts: res.data.slice(0,10)
+                })
+            })
+    }
+
+    render () {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className='center'>No posts yet</div>
+        )
+        return (
+            <div className='container'>
+                <h4 className='center'>Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = (props) => {
 
 
     // need to use high order component withRouter, make sure to import it as well.
-    setTimeout(()=>{
-        props.history.push('./about')
-    }, 2000);
+    // setTimeout(()=>{
+    //     props.history.push('./about')
+    // }, 2000);
 
     return (
         <nav className="nav-wrapper red darken-3">
