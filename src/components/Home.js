4003938847e2ea6515c2169ca7b2a4bb55f39c70
Firus Hanov <d00363593@dmail.dixie.edu>
2020-06-17 19:25:43 -0600

    delete button, redux implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
 
 class Home extends Component {
 
-    state = {
-        posts: [ ]
-    }
 
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        // returns a promise since it is asychnonious
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    posts: res.data.slice(0,10)
-                })
-            })
-    }
+
 
     render () {
-        const { posts } = this.state;
+        console.log(this.props)
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
@@ -49,4 +38,10 @@ class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home)
